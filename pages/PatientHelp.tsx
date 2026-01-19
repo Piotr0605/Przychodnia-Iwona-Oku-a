06bfa961a8b1ca459b3b1cee30d123818fcad7fe
Piotr0605 <sienkiewicz135@gmail.com>
2026-01-19 17:46:11 +0100

    Update

--- pages/PatientHelp.tsx
+++ pages/PatientHelp.tsx
@@ -34,12 +34,12 @@ export const PatientHelp: React.FC = () => {
                 <div className={`p-4 rounded-lg border ${isHighContrast ? 'border-gray-700' : 'bg-gray-50 border-gray-100'}`}>
                     <h3 className="font-bold text-lg mb-1">Szpital Wojewódzki w Białymstoku</h3>
                     <p>ul. Marii Skłodowskiej-Curie 26</p>
-                    <a href="tel:857488126" className="text-brand-600 font-bold hover:underline">Tel: 85 748 81 00</a>
+                    <a href="tel:857488100" className="text-brand-600 font-bold hover:underline">Tel: 85 748 81 00</a>
                 </div>
                 <div className={`p-4 rounded-lg border ${isHighContrast ? 'border-gray-700' : 'bg-gray-50 border-gray-100'}`}>
                     <h3 className="font-bold text-lg mb-1">USK (Uniwersytecki Szpital Kliniczny)</h3>
                     <p>ul. Marii Skłodowskiej-Curie 24a</p>
-                    <a href="tel:858316333" className="text-brand-600 font-bold hover:underline">Tel: 85 831 80 00</a>
+                    <a href="tel:858318000" className="text-brand-600 font-bold hover:underline">Tel: 85 831 80 00</a>
                 </div>
             </div>
           </div>
